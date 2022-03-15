1653635297be3dc4043367f3d487eda788c27df0
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-03-15 18:52:41 +0100

    front controller fixed

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,20 +1,26 @@
 <?php
 $wnd_title = array(
-    'title'=>'Polonia Online',
-    'motto'=>'Országos Lengyel Önkormányzat | Ogólnokrajowy Samorząd Polski na Węgrzech'
+	'title'=>'Polonia Online',
+	'motto'=>'Országos Lengyel Önkormányzat | Ogólnokrajowy Samorząd Polski na Węgrzech'
+);
+
+$header = array(
+	'img-source'=>'biglogo.jpg',
+	'img-alt'=>'logo'
 );
 
 $footer = array(
 	'copyright'=>'Copyright '.date('Y').'.',
-	'company'=>'Ádám Várhegyi-Milos'
+	'company'=>'Ádám Várhegyi-Miłoś (Y1JE9I)',
+	'original'=>'www.polonia.hu'
 );
 
 $pages = array(
-    '/'=>array('file'=>'cimlap','text'=>'Címlap'),
-    'onkormanyzat'=>array('file'=>'onkormanyzat','text'=>'Önkormányzat'),
-    'galeria'=>array('file'=>'galeria','text'=>'Galéria'),
-    'videok'=>array('file'=>'videok','text'=>'Videók'),
-    'kapcsolat'=>array('file'=>'kapcsolat','text'=>'Kapcsolat')
+	'/'=>array('file'=>'cimlap','text'=>'Címlap'),
+	'onkormanyzat'=>array('file'=>'onkormanyzat','text'=>'Önkormányzat'),
+	'galeria'=>array('file'=>'galeria','text'=>'Galéria'),
+	'videok'=>array('file'=>'videok','text'=>'Videók'),
+	'kapcsolat'=>array('file'=>'kapcsolat','text'=>'Kapcsolat')
 );
 
 $error_page = array('file'=>'404','text'=>'A keresett oldal nem található');
--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -2,3 +2,4 @@
 <p>
 	A magyarországi lengyelség érdekképviseletének ellátására az Országos Lengyel Önkormányzat 1995-ben alakult. (Régi neve: Országos Lengyel Kisebbségi Önkormányzat) (2011. évi CLXXIX. törvény a nemzetiségek jogairól). Az Országos Lengyel Önkormányzat már három intézménnyel dicsekedhet: az 1998 óta működő Magyarországi Lengyelek Múzeuma és Levéltárával, a 2004-ben alapított és szétszórtan az egész ország területén működő Országos Lengyel Nyelvoktató Iskolával, valamint az 1998-ban visszaszerzett (korábban államosított) Lengyel Házzal.
 </p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10784.919712802159!2d19.128534!3d47.485434!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dcca9bf7a9f7%3A0x4162679d137bd763!2zQnVkYXBlc3QsIMOBbGxvbcOhcyB1LiAxMCwgMTEwMiBNYWd5YXJvcnN6w6Fn!5e0!3m2!1shu!2sus!4v1647362247679!5m2!1shu!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
