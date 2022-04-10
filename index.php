1afffda1a93e502feb96b848fa676e4fb5d14bf3
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-04-10 17:25:59 +0200

    final menu structure
    added the "kapcsolat", "ujuzenet", "osszuzenet" site to the menu

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 include('./includes/config.inc.php');
-echo "SERVER['REQUEST_URI']: ".$_SERVER['REQUEST_URI'];
-if (isset($_GET['page'])) {
-	echo " ||| GET['page']: ".$_GET['page'];
-} else {
-	echo " ||| GET not set";
-}
-//$url = $_SERVER['REQUEST_URI'];
-//$stringArray = explode("/",$url);
-//$page = $stringArray[count($stringArray)-1];
 $search = $pages['/'];
 if (isset($_GET['page'])) {
 	if (isset($pages[$_GET['page']]) && file_exists("./templates/pages/{$pages[$_GET['page']]['file']}.tpl.php")) {
