56070f457f90fa3b33ca13e282d2af2c5e4ad1b1
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-04-10 17:01:19 +0200

    contacts on main page
    Merge contacts into main page. Deleted unnecessary Önkormányzat and Videók sites. Updated config.inc.php accordingly

---
+++ templates/pages/archivum.tpl.php
@@ -0,0 +1,20 @@
+<h3>A Lem emlékév fináléja</h3>
+<p>A Polonia Nova Kulturális Egyesület közösen a Lengyel Nagykövetség mellett működő Lengyel Iskolával szeretettel
+    várnak minden érdeklődőt a LEM-OFF.2021-re, vagyis a Lem emlékév fináléjára, amely november 3-án 18.00-kor lesz
+    Budapesten.</p>
+<p>Összegzik azokat a programokat, amelyek a LEM-ON.2021 keretében lettek meghirdetve, valamint kihirdetik a
+    „Cyberstwory” (Kiberszörnyek) című vetélkedő végeredményét.</p>
+<p>A rendezvény a Lengyel Nagykövetség mellett működő Lengyel Iskolában lesz, amelynek címe: 1025 Budapest, Törökvész út
+    15.</p>
+<p>Jelentkezésüket kérjük jelezzék a <a>novapolonia@gmail.com</p> e-mail címen. Az esemény a Segítség a Keleten Élő
+Lengyeleknek Alapítvány finanszírozásában került megrendezésre.</p>
+<h3>Ünnepeljük együtt a függetlenséget</h3>
+<p>ARTMUSICARTIST kamarazenei hangverseny</p>
+<p>Időpont: 2021 november 14., 11.30</p>
+<p>Helyszín: Budapest, Lengyel Ház, X. kerület, Óhegy u. 11</p>
+<p><a href="https://www.polonia.hu/images/csatolmanyok/2021/plakat_koncertow_razem_swietujemy_niepodleglosc.jpg"
+        target="_blank">MEGHÍVÓ</a></p>
+<h3>Andrzej Bator koncertje</h3>
+<p>Dátum: 2021 november 6, 16:00 óra</p>
+<p>Helyszín: 1051 Budapest, Nádor utca 34</p>
+<p><a href="https://www.polonia.hu/images/csatolmanyok/2021/bator.jpg" target="_blank">MEGHÍVÓ</a></p>
--- templates/pages/belepes.tpl.php
+++ templates/pages/belepes.tpl.php
@@ -1,22 +1,27 @@
-<form action = "?page=belep" method = "post">
+<form action="?page=belep" method="post">
 	<fieldset>
 		<legend>Bejelentkezés</legend>
 		<br>
 		<input type="text" name="username" placeholder="felhasználónév" pattern="^[1-9a-zA-Z-_.]{3,}$" required><br><br>
-		<input type="password" name="password" placeholder="jelszó" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$" required><br><br>
+		<input type="password" name="password" placeholder="jelszó"
+			pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$" required><br><br>
 		<input type="submit" name="login" value="Belépés">
 		<br>&nbsp;
 	</fieldset>
 </form>
 <h3>Regisztrálja magát, ha még nem felhasználó!</h3>
-<form action = "?page=regisztral" method = "post">
+<form action="?page=regisztral" method="post">
 	<fieldset>
 		<legend>Regisztráció</legend>
 		<br>
-		<input type="text" name="lastname" placeholder="vezetéknév" pattern="(?i)^(?:(?![×Þß÷þø])[-'0-9a-zÀ-ÿ])+$" required><br><br>
-		<input type="text" name="firstname" placeholder="utónév" pattern="(?i)^(?:(?![×Þß÷þø])[-'0-9a-zÀ-ÿ])+$" required><br><br>
-		<input type="text" name="username" placeholder="felhasználói név" pattern="^[1-9a-zA-Z-_.]{3,}$" required><br><br>
-		<input type="password" name="password" placeholder="jelszó" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$" required><br><br>
+		<input type="text" name="lastname" placeholder="vezetéknév" pattern="(?i)^(?:(?![×Þß÷þø])[-'0-9a-zÀ-ÿ])+$"
+			required><br><br>
+		<input type="text" name="firstname" placeholder="utónév" pattern="(?i)^(?:(?![×Þß÷þø])[-'0-9a-zÀ-ÿ])+$"
+			required><br><br>
+		<input type="text" name="username" placeholder="felhasználói név" pattern="^[1-9a-zA-Z-_.]{3,}$"
+			required><br><br>
+		<input type="password" name="password" placeholder="jelszó"
+			pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$" required><br><br>
 		<input type="submit" name="register" value="Regisztráció">
 		<br>&nbsp;
 	</fieldset>
