5450bf444ab66bb864dc7273c1c67116df7fa674
zsoltkecskemeti <101826444+zsoltkecskemeti@users.noreply.github.com>
2022-04-20 22:05:16 +0200

    js, index , css, ujuzenet

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <h1 class="intro">Írjon nekünk...</h1>
     <form name="kapcsolat" action="?page=ujuzenet" onsubmit="return ellenoriz();" method="post">
 
@@ -14,25 +13,3 @@
             <button onclick="ellenoriz();" type="button">Ellenőriz</button>
         </div>
     </form>
-=======
-<h1>Kapcsolat</h1>
-
- <form name="kapcsolat" action="?page=ujuzenet" onsubmit="return ellenoriz();" method="post">
- <div>
-
- <label><input type="text" id="nev" name="nev" size="20" maxlength="40">Név (minimum 5 
-karakter): </label>
- <br/>
- <label><input type="text" id="email" name="email" size="30" maxlength="40">E-mail 
-(kötelező): </label>
- <br/>
- <label> <textarea id="szoveg" name="szoveg" cols="40" rows="10"></textarea> Üzenet 
-(kötelező): </label>
- <br/>
-
- <input id="kuld" type="submit" value="Küld">
-
- <button onclick="ellenoriz();" type="button">Ellenőriz</button>
- </div>
- </form>
->>>>>>> Stashed changes
